9a17a5eae591b37de933cfd39b1dbab2d6491964
Eugene Tarlovskij <eugene.tarlovskij@gmail.com>
2016-04-19 15:40:35 +0300

    Database seeder correctly initializes database with sample data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,32 +12,46 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        $obj_people = json_decode(file_get_contents('members.txt'),true);
+        DB::delete('delete from meetups_topics');
+        DB::delete('delete from content');
+        DB::delete('delete from topics');
+        DB::delete('delete from people');
+        DB::delete('delete from meetups');
+        DB::delete('delete from places');
+        $obj_people = json_decode(file_get_contents('/var/www/database/seeds/members.txt'),true);
         foreach($obj_people as $k => $v)
         {
-            # print_r($v);
-            DB::table('people')->insertIgnore(['name'=>$v['name'],'vk'=>$v['vk'],'github'=>$v['github']]);
+            DB::table('people')->insert(['name'=>$v['name'],'vk'=>$v['vk'],'github'=>$v['github']]);
         }
 
-        $obj_meetups = json_decode(file_get_contents('meetups.txt'),true);
+        $obj_meetups = json_decode(file_get_contents('/var/www/database/seeds/meetups.txt'),true);
         foreach($obj_meetups as $k => $v)
         {
-            # print_r($v);
-            DB::table('places')->insertIgnore(['name'=>$v['place'],'longitude'=>$v['lng'],'latitude'=>$v['lat']]);
             $placeId = DB::table('places')->select('id')->where('name','=',$v['place'])->get();
-            DB::table('meetups')->insertIgnore(['datetime'=>$v['datetime'], 'place_id'=>$placeId, 'note'=>$v['note']]);
+            if (count($placeId)==0)
+            {
+                DB::table('places')->insert(['name'=>$v['place'],'longitude'=>strval($v['lgt']),'latitude'=>strval($v['lat'])]);
+                $placeId = DB::table('places')->select('id')->where('name','=',$v['place'])->get();
+            }
+            $placeId = $placeId[0]->{'id'};
+            DB::table('meetups')->insert(['datetime'=>strval($v['datetime']), 'place_id'=>strval($placeId), 'note'=>$v['note']]);
             foreach($v['topics'] as $nv)
             {
-                DB::table('topics')->insertIgnore(['title'=>$nv['title']]);
+                DB::table('topics')->insert(['title'=>$nv['title']]);
                 $topicId = DB::table('topics')->select('id')->where('title','=',$nv['title'])->get();
+                $topicId = $topicId[0]->{'id'};
                 $ordNr = 1;
-                foreach($nv['content'] as $cv)
+                if (array_key_exists('content',$nv))
                 {
-                    DB::table('content')->insertIgnore(['topic_id'=>$topicId, 'ordinal_nr'=>$ordNr, 'url'=>$cv['url'],'title'=>$cv['title']]);
-                    $ordNr = $ordNr + 1;
+                    foreach($nv['content'] as $cv)
+                    {
+                        DB::table('content')->insert(['topic_id'=>strval($topicId), 'ordinal_nr'=>strval($ordNr), 'url'=>$cv['url'],'title'=>$cv['title']]);
+                        $ordNr = $ordNr + 1;
+                    }
                 }
                 $authorId = DB::table('people')->select('id')->where('name','=',$nv['auname'])->get();
-                DB::table('meetups_topics')->insertIgnore(['meetup_date'=>$v['datetime'],'topic_id'=>$topicId,'person_id'=>$authorId]);
+                $authorId = $authorId[0]->{'id'};
+                DB::table('meetups_topics')->insert(['meetup_date'=>$v['datetime'],'topic_id'=>$topicId,'person_id'=>$authorId]);
             }
         }
     }
